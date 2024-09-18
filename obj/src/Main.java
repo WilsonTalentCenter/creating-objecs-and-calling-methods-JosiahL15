e6d56f467859e4917f8af8dd98379b0152dd1904
JosiahL15 <jl0001@students.inghamisd.org>
2024-09-18 12:12:53 -0400

    yippee

--- obj/src/Main.java
+++ obj/src/Main.java
@@ -16,11 +16,16 @@ public class Main {
 // call the "before" method on currentDate passing in anotherDate  put the returned value into a boolean variable called isBefore.
         boolean isBefore = currentDate.before(anotherDate);
 // print the value of isBefore followed by a newline character
-        System.out.println("isBefore");
+        System.out.println(isBefore);
 ////////////////
 //create a Scanner object called myScanner,  pass in System.in to as the parameter to the constructor
         Scanner myScanner = new Scanner(System.in);
 //call the "nextLine" method on the myScanner object save the result into a string variable called resString
         String resString = myScanner.nextLine();
 //print resString followed by a newline character
-        System.out.println("resString");}}
+        System.out.println(resString);
+        //call the "next" method on the myScanner object saving the result into resString
+        resString = myScanner.next();
+        //print resString followed by a newline character
+        System.out.println(resString);
+    }}
